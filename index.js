1d322a076917a4fb131ae378af4c12d05541b490
Lior Neu-ner <lior@searcle.com>
2017-06-03 17:00:04 +0200

    updated the bot to receive message

--- index.js
+++ index.js
@@ -34,34 +34,62 @@ app.listen(app.get('port'), function() {
 })
 
 // Process messages
-app.post('/webhook/', function (req, res){
-        let messaging_events = req.body.entry[0].messaging
-            for (let i = 0; i < messaging_events.length; i++) {
-	    let event = req.body.entry[0].messaging[i]
-	    let sender = event.sender.id
-	    if (event.message && event.message.text) {
-		    let text = event.message.text
-		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-	    }
-    }
-    res.sendStatus(200)
-})
-
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
-	    method: 'POST',
-		json: {
-		    recipient: {id:sender},
-			message: messageData,
+app.post('/webhook/', function (req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 		}
-	}, function(error, response, body) {
-		if (error) {
-		    console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
-}
+
+	}
+	var data = req.body;
+
+	// Iterate over each entry - there may be multiple if batched
+	data.entry.forEach(function(entry) {
+		var pageID = entry.id;
+		var timeOfEvent = entry.time;
+
+		// Iterate over each messaging event
+		entry.messaging.forEach(function(event) {
+			if (event.message) {
+				receivedMessage(event);
+			} else {
+				console.log("Webhook received unknown event: ", event);
+			}
+		});
+
+		res.sendStatus(200)
+	})
+
+	function receivedMessage(event) {
+		var senderID = event.sender.id;
+		var recipientID = event.recipient.id;
+		var timeOfMessage = event.timestamp;
+		var message = event.message;
+
+		console.log("Received message for user %d and page %d at %d with message:",
+		senderID, recipientID, timeOfMessage);
+		console.log(JSON.stringify(message));
+	}
+
+	function sendTextMessage(sender, text) {
+		let messageData = { text:text }
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			qs: {access_token:token},
+			method: 'POST',
+			json: {
+				recipient: {id:sender},
+				message: messageData,
+			}
+		}, function(error, response, body) {
+			if (error) {
+				console.log('Error sending messages: ', error)
+			} else if (response.body.error) {
+				console.log('Error: ', response.body.error)
+			}
+		})
+	}
