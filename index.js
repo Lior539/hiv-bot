0d88d84128be1c1bf7640db328d45c86cdb47a39
Lior Neu-ner <lior@searcle.com>
2017-06-18 17:52:41 +0200

    added back semi colons for 'for loop'

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.listen(app.get('port'), function() {
 // Process messages
 app.post('/webhook/', function (req, res) {
 	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0  i < messaging_events.length  i++) {
+	for (let i = 0;  i < messaging_events.length;  i++) {
 		let event = req.body.entry[0].messaging[i]
 		forwardMessengerEventToWit(event)
 	}
