d8a15fcf9e8364766786c3c964bd76aabcef44b1
Lior Neu-ner <lior@searcle.com>
2017-06-18 17:50:29 +0200

    removed semicolons

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const FB_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN
 const WIT_TOKEN = process.env.WIT_AI_SERVER_TOKEN
 const FB_HUB_VERIFY_TOKEN = process.env.FB_HUB_VERIFY_TOKEN
 
-let Wit = null;
-let log = null;
+let Wit = null
+let log = null
 try {
 	// if running from repo
-	Wit = require('../').Wit;
-	log = require('../').log;
+	Wit = require('../').Wit
+	log = require('../').log
 } catch (e) {
-	Wit = require('node-wit').Wit;
-	log = require('node-wit').log;
+	Wit = require('node-wit').Wit
+	log = require('node-wit').log
 }
 
 // ----------------------------------------------------------------------------
@@ -29,8 +29,8 @@ const fbMessage = (id, text) => {
 	const body = JSON.stringify({
 		recipient: { id },
 		message: { text },
-	});
-	const qs = 'access_token=' + encodeURIComponent(FB_TOKEN);
+	})
+	const qs = 'access_token=' + encodeURIComponent(FB_TOKEN)
 	return fetch('https://graph.facebook.com/v2.6/me/messages?' + qs, {
 		method: 'POST',
 		headers: {'Content-Type': 'application/json'},
@@ -39,11 +39,11 @@ const fbMessage = (id, text) => {
 	.then(rsp => rsp.json())
 	.then(json => {
 		if (json.error && json.error.message) {
-			throw new Error(json.error.message);
+			throw new Error(json.error.message)
 		}
-		return json;
-	});
-};
+		return json
+	})
+}
 
 // ----------------------------------------------------------------------------
 // Wit.ai bot specific code
@@ -52,31 +52,31 @@ const fbMessage = (id, text) => {
 // Each session has an entry:
 // sessionId -> {fbid: facebookUserId, context: sessionState}
 
-const sessions = {};
+const sessions = {}
 
 const findOrCreateSession = (fbid) => {
-	let sessionId;
+	let sessionId
 	// Let's see if we already have a session for the user fbid
 	Object.keys(sessions).forEach(k => {
 		if (sessions[k].fbid === fbid) {
 			// Yep, got it!
-			sessionId = k;
+			sessionId = k
 		}
-	});
+	})
 	if (!sessionId) {
 		// No session found for user fbid, let's create a new one
-		sessionId = new Date().toISOString();
-		sessions[sessionId] = {fbid: fbid, context: {}};
+		sessionId = new Date().toISOString()
+		sessions[sessionId] = {fbid: fbid, context: {}}
 	}
-	return sessionId;
-};
+	return sessionId
+}
 
 // Our bot actions
 const actions = {
 	send({sessionId}, {text}) {
 		// Our bot has something to say!
 		// Let's retrieve the Facebook user whose session belongs to
-		const recipientId = sessions[sessionId].fbid;
+		const recipientId = sessions[sessionId].fbid
 		if (recipientId) {
 			// Yay, we found our recipient!
 			// Let's forward our bot response to her.
@@ -89,24 +89,24 @@ const actions = {
 					recipientId,
 					':',
 					err.stack || err
-				);
-			});
+				)
+			})
 		} else {
-			console.error('Oops! Couldn\'t find user for session:', sessionId);
+			console.error('Oops! Couldn\'t find user for session:', sessionId)
 			// Giving the wheel back to our bot
 			return Promise.resolve()
 		}
 	},
 	// You should implement your custom actions here
 	// See https://wit.ai/docs/quickstart
-};
+}
 
 // Setting up our bot
 const wit = new Wit({
 	accessToken: WIT_TOKEN,
 	actions,
 	logger: new log.Logger(log.INFO)
-});
+})
 
 // ----------------------------------------------------------------------------
 const app = express()
@@ -114,10 +114,10 @@ app.set('port', (process.env.PORT || 5000))
 
 app.use(({method, url}, rsp, next) => {
 	rsp.on('finish', () => {
-		console.log(`${rsp.statusCode} ${method} ${url}`);
-	});
-	next();
-});
+		console.log(`${rsp.statusCode} ${method} ${url}`)
+	})
+	next()
+})
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
@@ -146,10 +146,9 @@ app.listen(app.get('port'), function() {
 // Process messages
 app.post('/webhook/', function (req, res) {
 	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
+	for (let i = 0  i < messaging_events.length  i++) {
 		let event = req.body.entry[0].messaging[i]
 		forwardMessengerEventToWit(event)
-		receivedMessengerEvent(event)
 	}
 	res.sendStatus(200)
 })
@@ -162,7 +161,7 @@ function forwardMessengerEventToWit(event) {
 	let messageText = event.message.text
 
 	let senderId = event.sender.id
-	let sessionId = findOrCreateSession(senderId);
+	let sessionId = findOrCreateSession(senderId)
 
 	wit.message(
 		messageText
@@ -170,35 +169,35 @@ function forwardMessengerEventToWit(event) {
 		handleWitSuccessResponse(context, senderId, sessionId, messageText)
 	})
 	.catch((err) => {
-		console.error('Oops! Got an error from Wit: ', err.stack || err);
+		console.error('Oops! Got an error from Wit: ', err.stack || err)
 	})
 }
 
 function handleWitSuccessResponse(context, fbSenderId, sessionId, originalMessage) {
 	// Our bot did everything it has to do.
 	// Now it's waiting for further messages to proceed.
-	console.log('Waiting for next user messages');
-	console.log('Context Entities :', context.entities);
-	let entities = context.entities;
-	var messageToSend = '';
+	console.log('Waiting for next user messages')
+	console.log('Context Entities :', context.entities)
+	let entities = context.entities
+	var messageToSend = ''
 	if (entities.count != 1) {
-		console.log('Context entities for message \"', originalMessage, '\" does not equal 1 for context: ', context);
-		messageToSend = 'I \'m not sure I understand what you\'re asking. You can try calling the Toll-Free HIV and AIDS Helpline and speak to a human - 0800-012-322';
+		console.log('Context entities for message \"', originalMessage, '\" does not equal 1 for context: ', context)
+		messageToSend = 'I \'m not sure I understand what you\'re asking. You can try calling the Toll-Free HIV and AIDS Helpline and speak to a human - 0800-012-322'
 	} else {
-		let entity = entities[0];
-		messageToSend = 'I got back an entity called ' + entity.term;
+		let entity = entities[0]
+		messageToSend = 'I got back an entity called ' + entity.term
 		// Based on the session state, you might want to reset the session.
 		// This depends heavily on the business logic of your bot.
 		// Example:
 		// if (context['done']) {
-		//   delete sessions[sessionId];
+		//   delete sessions[sessionId]
 		// }
 
 		// Updating the user's current session state
-		sessions[sessionId].context = context;
+		sessions[sessionId].context = context
 	}
 
-	sendMessengerTextMessageToUserWithId(fbSenderId, messageToSend);
+	sendMessengerTextMessageToUserWithId(fbSenderId, messageToSend)
 }
 
 // ----------------------------------------------------------------------------
@@ -219,8 +218,8 @@ function sendMessengerTextMessageToUserWithId(id, text) {
 		} else if (response.body.error) {
 			console.log('Error: ', response.body.error)
 		} else {
-			console.log("Sent text message to user ", id);
-			console.log("Message", messageData);
+			console.log("Sent text message to user ", id)
+			console.log("Message", messageData)
 		}
 	})
 }
